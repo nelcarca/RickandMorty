9ac2dad559cf6ecfed04e19d804794b7295c3b1c
Nelson Carvajal <nelcarca@gmail.com>
2022-02-08 16:17:50 -0300

    PersonajesOk

--- js/apiCharacter.js
+++ js/apiCharacter.js
@@ -1,5 +1,12 @@
 
 const nameCharacter = document.querySelector("#titulo");
+const imgCharacter = document.querySelector('#imgCharacter');
+const statusCharacter = document.querySelector('#statusCharacter');
+const genderCharacter = document.querySelector ('#genderCharacter');
+const speciesCharacter = document.querySelector ('#speciesCharacter');
+const originCharacter = document.querySelector ('#originCharacter');
+const locationCharacter = document.querySelector ('#locationCharacter');
+
 
 const parametro = window.location.search;
 const urlParams = new URLSearchParams(parametro);
@@ -18,10 +25,30 @@ const getCharacterById = async (id) => {
 
 //el llamado a la funcion
 getCharacterById(id).then( data => {
-   nameCharacter.innerText = data.name
+   nameCharacter.innerText = data.name;
+   imgCharacter.src = data.image;
+   genderCharacter.innerText = "Genero: " + data.gender;
+   speciesCharacter.innerText = "Especie: " + data.species;
+   originCharacter.innerText = "Origen: " + data.origin.name;
+   locationCharacter.innerText = "Ubicación actual: " + data.location.name;
+   statusCharacter.innerText = "Condición: " + data.status;
+   if (data.status === 'Alive') {
+      statusCharacter.classList.add('green');
+   } else if (data.status === 'Dead') {
+      statusCharacter.classList.add('red');
+   } else {
+      statusCharacter.classList.add('yellow');
+   }
+
 }).catch(err => err);
+
+
+// getCharacterById(id).then( data => {
+   
+// }).catch(err => err);
+
 //peticion al api pero por id
 
-const printPagination = (info) => {
+// const printPagination = (info) => {
 
-}
+// }
